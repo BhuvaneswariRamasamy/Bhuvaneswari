3f80adc42fc8c3bf78bddda4267c9f347730ae03
BhuvaneswariRamasamy <bhuvanakeerthi1197@gmail.com>
2018-05-29 11:13:27 +0530

    pro

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld{
+public static void main(String s[]){
+System.out.println("Welcome");
+short one=100;
+
+short two=200;
+int three=one+two;
+System.out.println(three);
+}
+}
